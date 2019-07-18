1b39f68f3689016c7de1e1a51f2d8767386ac7a6
chang <qi.chang1022@gmail.com>
2019-07-18 15:21:43 +0200

    --add example
    Signed-off-by: chang <qi.chang1022@gmail.com>

--- py_packaging/py_packaging.py
+++ py_packaging/py_packaging.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
 
 """Main module."""
+def hello_pypackage():
+	print("Hello there  {{cookiecutter.location}} !") 
+	file = pd.read_csv("../data/example.csv", sep=";", encoding="utf-8", header=None)
+	print(file)
+	
+	
+if __name__ == '__main__':
+	hello_pypackage()
